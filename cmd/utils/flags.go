luxfi/coreth

Update import path

cmd/utils/flags.go
@@ -35,46 +35,46 @@ import (
 	"strings"
 	"time"
 
-	"github.com/ethereum/go-ethereum/accounts"
-	"github.com/ethereum/go-ethereum/accounts/keystore"
-	bparams "github.com/ethereum/go-ethereum/beacon/params"
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/common/fdlimit"
-	"github.com/ethereum/go-ethereum/common/hexutil"
-	"github.com/ethereum/go-ethereum/core"
-	"github.com/ethereum/go-ethereum/core/rawdb"
-	"github.com/ethereum/go-ethereum/core/txpool/blobpool"
-	"github.com/ethereum/go-ethereum/core/txpool/legacypool"
-	"github.com/ethereum/go-ethereum/core/vm"
-	"github.com/ethereum/go-ethereum/crypto"
-	"github.com/ethereum/go-ethereum/crypto/kzg4844"
-	"github.com/ethereum/go-ethereum/eth"
-	"github.com/ethereum/go-ethereum/eth/catalyst"
-	"github.com/ethereum/go-ethereum/eth/ethconfig"
-	"github.com/ethereum/go-ethereum/eth/filters"
-	"github.com/ethereum/go-ethereum/eth/gasprice"
-	"github.com/ethereum/go-ethereum/eth/tracers"
-	"github.com/ethereum/go-ethereum/ethdb"
-	"github.com/ethereum/go-ethereum/ethdb/remotedb"
-	"github.com/ethereum/go-ethereum/ethstats"
-	"github.com/ethereum/go-ethereum/graphql"
-	"github.com/ethereum/go-ethereum/internal/ethapi"
-	"github.com/ethereum/go-ethereum/internal/flags"
-	"github.com/ethereum/go-ethereum/log"
-	"github.com/ethereum/go-ethereum/metrics"
-	"github.com/ethereum/go-ethereum/metrics/exp"
-	"github.com/ethereum/go-ethereum/metrics/influxdb"
-	"github.com/ethereum/go-ethereum/miner"
-	"github.com/ethereum/go-ethereum/node"
-	"github.com/ethereum/go-ethereum/p2p"
-	"github.com/ethereum/go-ethereum/p2p/enode"
-	"github.com/ethereum/go-ethereum/p2p/nat"
-	"github.com/ethereum/go-ethereum/p2p/netutil"
-	"github.com/ethereum/go-ethereum/params"
-	"github.com/ethereum/go-ethereum/rpc"
-	"github.com/ethereum/go-ethereum/triedb"
-	"github.com/ethereum/go-ethereum/triedb/hashdb"
-	"github.com/ethereum/go-ethereum/triedb/pathdb"
+	"github.com/luxfi/geth/accounts"
+	"github.com/luxfi/geth/accounts/keystore"
+	bparams "github.com/luxfi/geth/beacon/params"
+	"github.com/luxfi/geth/common"
+	"github.com/luxfi/geth/common/fdlimit"
+	"github.com/luxfi/geth/common/hexutil"
+	"github.com/luxfi/geth/core"
+	"github.com/luxfi/geth/core/rawdb"
+	"github.com/luxfi/geth/core/txpool/blobpool"
+	"github.com/luxfi/geth/core/txpool/legacypool"
+	"github.com/luxfi/geth/core/vm"
+	"github.com/luxfi/geth/crypto"
+	"github.com/luxfi/geth/crypto/kzg4844"
+	"github.com/luxfi/geth/eth"
+	"github.com/luxfi/geth/eth/catalyst"
+	"github.com/luxfi/geth/eth/ethconfig"
+	"github.com/luxfi/geth/eth/filters"
+	"github.com/luxfi/geth/eth/gasprice"
+	"github.com/luxfi/geth/eth/tracers"
+	"github.com/luxfi/geth/ethdb"
+	"github.com/luxfi/geth/ethdb/remotedb"
+	"github.com/luxfi/geth/ethstats"
+	"github.com/luxfi/geth/graphql"
+	"github.com/luxfi/geth/internal/ethapi"
+	"github.com/luxfi/geth/internal/flags"
+	"github.com/luxfi/geth/log"
+	"github.com/luxfi/geth/metrics"
+	"github.com/luxfi/geth/metrics/exp"
+	"github.com/luxfi/geth/metrics/influxdb"
+	"github.com/luxfi/geth/miner"
+	"github.com/luxfi/geth/node"
+	"github.com/luxfi/geth/p2p"
+	"github.com/luxfi/geth/p2p/enode"
+	"github.com/luxfi/geth/p2p/nat"
+	"github.com/luxfi/geth/p2p/netutil"
+	"github.com/luxfi/geth/params"
+	"github.com/luxfi/geth/rpc"
+	"github.com/luxfi/geth/triedb"
+	"github.com/luxfi/geth/triedb/hashdb"
+	"github.com/luxfi/geth/triedb/pathdb"
 	pcsclite "github.com/gballet/go-libpcsclite"
 	gopsutil "github.com/shirou/gopsutil/mem"
 	"github.com/urfave/cli/v2"

luxfi/geth

cmd/utils/flags.go
@@ -35,46 +35,46 @@ import (
 	"strings"
 	"time"
 
-	"github.com/ethereum/go-ethereum/accounts"
-	"github.com/ethereum/go-ethereum/accounts/keystore"
-	bparams "github.com/ethereum/go-ethereum/beacon/params"
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/common/fdlimit"
-	"github.com/ethereum/go-ethereum/common/hexutil"
-	"github.com/ethereum/go-ethereum/core"
-	"github.com/ethereum/go-ethereum/core/rawdb"
-	"github.com/ethereum/go-ethereum/core/txpool/blobpool"
-	"github.com/ethereum/go-ethereum/core/txpool/legacypool"
-	"github.com/ethereum/go-ethereum/core/vm"
-	"github.com/ethereum/go-ethereum/crypto"
-	"github.com/ethereum/go-ethereum/crypto/kzg4844"
-	"github.com/ethereum/go-ethereum/eth"
-	"github.com/ethereum/go-ethereum/eth/ethconfig"
-	"github.com/ethereum/go-ethereum/eth/filters"
-	"github.com/ethereum/go-ethereum/eth/gasprice"
-	"github.com/ethereum/go-ethereum/eth/syncer"
-	"github.com/ethereum/go-ethereum/eth/tracers"
-	"github.com/ethereum/go-ethereum/ethdb"
-	"github.com/ethereum/go-ethereum/ethdb/remotedb"
-	"github.com/ethereum/go-ethereum/ethstats"
-	"github.com/ethereum/go-ethereum/graphql"
-	"github.com/ethereum/go-ethereum/internal/ethapi"
-	"github.com/ethereum/go-ethereum/internal/flags"
-	"github.com/ethereum/go-ethereum/log"
-	"github.com/ethereum/go-ethereum/metrics"
-	"github.com/ethereum/go-ethereum/metrics/exp"
-	"github.com/ethereum/go-ethereum/metrics/influxdb"
-	"github.com/ethereum/go-ethereum/miner"
-	"github.com/ethereum/go-ethereum/node"
-	"github.com/ethereum/go-ethereum/p2p"
-	"github.com/ethereum/go-ethereum/p2p/enode"
-	"github.com/ethereum/go-ethereum/p2p/nat"
-	"github.com/ethereum/go-ethereum/p2p/netutil"
-	"github.com/ethereum/go-ethereum/params"
-	"github.com/ethereum/go-ethereum/rpc"
-	"github.com/ethereum/go-ethereum/triedb"
-	"github.com/ethereum/go-ethereum/triedb/hashdb"
-	"github.com/ethereum/go-ethereum/triedb/pathdb"
+	"github.com/luxfi/geth/accounts"
+	"github.com/luxfi/geth/accounts/keystore"
+	bparams "github.com/luxfi/geth/beacon/params"
+	"github.com/luxfi/geth/common"
+	"github.com/luxfi/geth/common/fdlimit"
+	"github.com/luxfi/geth/common/hexutil"
+	"github.com/luxfi/geth/core"
+	"github.com/luxfi/geth/core/rawdb"
+	"github.com/luxfi/geth/core/txpool/blobpool"
+	"github.com/luxfi/geth/core/txpool/legacypool"
+	"github.com/luxfi/geth/core/vm"
+	"github.com/luxfi/geth/crypto"
+	"github.com/luxfi/geth/crypto/kzg4844"
+	"github.com/luxfi/geth/eth"
+	"github.com/luxfi/geth/eth/ethconfig"
+	"github.com/luxfi/geth/eth/filters"
+	"github.com/luxfi/geth/eth/gasprice"
+	"github.com/luxfi/geth/eth/syncer"
+	"github.com/luxfi/geth/eth/tracers"
+	"github.com/luxfi/geth/ethdb"
+	"github.com/luxfi/geth/ethdb/remotedb"
+	"github.com/luxfi/geth/ethstats"
+	"github.com/luxfi/geth/graphql"
+	"github.com/luxfi/geth/internal/ethapi"
+	"github.com/luxfi/geth/internal/flags"
+	"github.com/luxfi/geth/log"
+	"github.com/luxfi/geth/metrics"
+	"github.com/luxfi/geth/metrics/exp"
+	"github.com/luxfi/geth/metrics/influxdb"
+	"github.com/luxfi/geth/miner"
+	"github.com/luxfi/geth/node"
+	"github.com/luxfi/geth/p2p"
+	"github.com/luxfi/geth/p2p/enode"
+	"github.com/luxfi/geth/p2p/nat"
+	"github.com/luxfi/geth/p2p/netutil"
+	"github.com/luxfi/geth/params"
+	"github.com/luxfi/geth/rpc"
+	"github.com/luxfi/geth/triedb"
+	"github.com/luxfi/geth/triedb/hashdb"
+	"github.com/luxfi/geth/triedb/pathdb"
 	pcsclite "github.com/gballet/go-libpcsclite"
 	gopsutil "github.com/shirou/gopsutil/mem"
 	"github.com/urfave/cli/v2"