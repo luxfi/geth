a22a4dd05e057ab0e804e96a3a6e3fd9f7d4332d
Zach Kelling <z@zeekay.io>
2025-08-03 01:17:04 +0000

    feat: Use dgraph-io/badger/v4 directly in badgerdb implementation
    - Remove dependency on luxfi/database wrapper for badgerdb
    - Implement ethdb.Database interface directly on top of badger v4
    - Update database dependency to v1.1.5
    - Simplify badgerdb implementation for better maintainability

--- crypto/signature_cgo.go
+++ crypto/signature_cgo.go
@@ -25,12 +25,12 @@ import (
 	"fmt"
 
 	"github.com/luxfi/geth/common/math"
-	"github.com/luxfi/geth/crypto/secp256k1"
+	luxSecp256k1 "github.com/luxfi/crypto/secp256k1"
 )
 
 // Ecrecover returns the uncompressed public key that created the given signature.
 func Ecrecover(hash, sig []byte) ([]byte, error) {
-	return secp256k1.RecoverPubkey(hash, sig)
+	return luxSecp256k1.RecoverPubkey(hash, sig)
 }
 
 // SigToPub returns the public key that created the given signature.
@@ -56,19 +56,19 @@ func Sign(digestHash []byte, prv *ecdsa.PrivateKey) (sig []byte, err error) {
 	}
 	seckey := math.PaddedBigBytes(prv.D, prv.Params().BitSize/8)
 	defer zeroBytes(seckey)
-	return secp256k1.Sign(digestHash, seckey)
+	return luxSecp256k1.Sign(digestHash, seckey)
 }
 
 // VerifySignature checks that the given public key created signature over digest.
 // The public key should be in compressed (33 bytes) or uncompressed (65 bytes) format.
 // The signature should have the 64 byte [R || S] format.
 func VerifySignature(pubkey, digestHash, signature []byte) bool {
-	return secp256k1.VerifySignature(pubkey, digestHash, signature)
+	return luxSecp256k1.VerifySignature(pubkey, digestHash, signature)
 }
 
 // DecompressPubkey parses a public key in the 33-byte compressed format.
 func DecompressPubkey(pubkey []byte) (*ecdsa.PublicKey, error) {
-	x, y := secp256k1.DecompressPubkey(pubkey)
+	x, y := luxSecp256k1.DecompressPubkey(pubkey)
 	if x == nil {
 		return nil, errors.New("invalid public key")
 	}
@@ -77,10 +77,10 @@ func DecompressPubkey(pubkey []byte) (*ecdsa.PublicKey, error) {
 
 // CompressPubkey encodes a public key to the 33-byte compressed format.
 func CompressPubkey(pubkey *ecdsa.PublicKey) []byte {
-	return secp256k1.CompressPubkey(pubkey.X, pubkey.Y)
+	return luxSecp256k1.CompressPubkey(pubkey.X, pubkey.Y)
 }
 
 // S256 returns an instance of the secp256k1 curve.
 func S256() EllipticCurve {
-	return secp256k1.S256()
+	return luxSecp256k1.S256()
 }
