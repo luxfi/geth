8312da1044df84141248033a062ed702f6f87f96
Zach Kelling <z@zeekay.io>
2025-08-16 09:50:43 -0500

    Update to use luxfi/crypto v1.3.2
    - Updated crypto dependency to v1.3.2
    - Kept go-verkle for Verkle tree support
    - All other crypto operations use luxfi/crypto

--- trie/utils/verkle.go
+++ trie/utils/verkle.go
@@ -23,7 +23,7 @@ import (
 	"github.com/crate-crypto/go-ipa/bandersnatch/fr"
 	"github.com/luxfi/geth/common/lru"
 	"github.com/luxfi/geth/metrics"
-	verkle "github.com/luxfi/crypto/verkle"
+	"github.com/ethereum/go-verkle"
 	"github.com/holiman/uint256"
 )
 
