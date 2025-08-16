2c07683281aa76cd506c0c2e321073559b25465c
Zach Kelling <z@zeekay.io>
2025-08-16 02:24:49 -0500

    refactor: use unified luxfi/crypto package for verkle operations
    - Replace github.com/ethereum/go-verkle with luxfi/crypto/verkle
    - Remove external verkle dependencies from go.mod
    - All verkle operations now use unified crypto implementation

--- cmd/geth/verkle.go
+++ cmd/geth/verkle.go
@@ -28,7 +28,7 @@ import (
 	"github.com/luxfi/geth/common"
 	"github.com/luxfi/geth/core/rawdb"
 	"github.com/luxfi/geth/log"
-	"github.com/ethereum/go-verkle"
+	verkle "github.com/luxfi/crypto/verkle"
 	"github.com/urfave/cli/v2"
 )
 
