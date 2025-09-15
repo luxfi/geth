090e2cc91c860241c8f2630a5d7404b70480890d
Zach Kelling <z@zeekay.io>
2025-09-15 01:17:53 +0000

    Revert "sync: merge latest upstream from ethereum/go-ethereum with lux customizations"
    This reverts commit 09e2398c8b24390434e2e2b2125dff03a3b6787b.

--- core/blockchain.go
+++ core/blockchain.go
@@ -389,9 +389,6 @@ func NewBlockChain(db ethdb.Database, genesis *Genesis, engine consensus.Engine,
 	bc.validator = NewBlockValidator(chainConfig, bc)
 	bc.prefetcher = newStatePrefetcher(chainConfig, bc.hc)
 	bc.processor = NewStateProcessor(chainConfig, bc.hc)
-	if processor, ok := bc.processor.(*StateProcessor); ok {
-		processor.SetVMConfig(&cfg.VmConfig)
-	}
 
 	genesisHeader := bc.GetHeaderByNumber(0)
 	if genesisHeader == nil {
@@ -2048,7 +2045,7 @@ func (bc *BlockChain) processBlock(parentRoot common.Hash, block *types.Block, s
 
 	// Process block using the parent state as reference point
 	pstart := time.Now()
-	res, err := bc.processor.Process(block, statedb, bc.cfg.VmConfig)
+	res, err := bc.processor.Process(block, statedb, bc.chainConfig)
 	if err != nil {
 		bc.reportBlock(block, res, err)
 		return nil, err
@@ -2056,7 +2053,7 @@ func (bc *BlockChain) processBlock(parentRoot common.Hash, block *types.Block, s
 	ptime := time.Since(pstart)
 
 	vstart := time.Now()
-	if err := bc.validator.ValidateState(block, statedb, res, false); err != nil {
+	if err := bc.validator.ValidateState(block, statedb, res.Receipts, res.GasUsed); err != nil {
 		bc.reportBlock(block, res, err)
 		return nil, err
 	}
