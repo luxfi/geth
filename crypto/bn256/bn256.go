f692a09078b00914b4c745bbe2d7f61f3bfd0971
Zach Kelling <z@zeekay.io>
2025-09-16 10:19:20 +0000

    fix: finalize upstream merge with bn256 wrapper and build guards for post-quantum code

---
+++ crypto/bn256/bn256.go
@@ -0,0 +1,32 @@
+// Copyright 2018 The go-ethereum Authors
+// This file is part of the go-ethereum library.
+//
+// The go-ethereum library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The go-ethereum library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
+
+// Package bn256 implements the Optimal Ate pairing over a 256-bit Barreto-Naehrig curve.
+package bn256
+
+import (
+	"github.com/luxfi/geth/crypto/bn256/cloudflare"
+)
+
+// Re-export the cloudflare implementation as the default
+type G1 = cloudflare.G1
+type G2 = cloudflare.G2
+type GT = cloudflare.GT
+
+// PairingCheck calculates the Optimal Ate pairing for a set of points.
+func PairingCheck(a []*G1, b []*G2) bool {
+	return cloudflare.PairingCheck(a, b)
+}
