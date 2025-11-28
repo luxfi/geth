9c629ea89ca40387b03be1ca4183e8d0185f9cec
Zach Kelling <z@zeekay.io>
2025-11-28 08:24:24 +0000

    chore: clean up geth - remove plugin/evm and broken cmd tools
    geth is a low-level go-ethereum fork that should not depend on
    higher-level luxfi packages like evm, node, consensus, etc.
    
    - Remove plugin/evm directory (belongs in luxfi/evm)
    - Remove plugin/main.go stub
    - Remove broken/outdated cmd tools for regenesis
    - Clean up go.mod to only require luxfi/crypto
    - Build and tests pass

--- plugin/evm/gossip/noop.go
+++
@@ -1,7 +0,0 @@
-// Copyright (C) 2019-2025, Lux Industries, Inc. All rights reserved.
-// See the file LICENSE for licensing terms.
-
-package gossip
-
-// Placeholder for gossip package
-type NoOpGossip struct{}
