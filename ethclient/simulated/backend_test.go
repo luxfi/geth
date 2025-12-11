a93760dc047af0ed0581bfe0ec9273c7a4ad91f8
Zach Kelling <z@zeekay.io>
2025-12-11 04:54:57 +0000

    refactor: use luxfi/crypto/kzg4844 instead of internal kzg4844
    Externalize kzg4844 to luxfi/crypto package for consistent crypto
    usage across all luxfi packages. The internal crypto/kzg4844 directory
    is removed as it's now provided by luxfi/crypto.
    
    Also updates Go to 1.25.5 and luxfi/crypto to v1.17.15.

--- ethclient/simulated/backend_test.go
+++ ethclient/simulated/backend_test.go
@@ -29,7 +29,7 @@ import (
 	"github.com/luxfi/geth/common"
 	"github.com/luxfi/geth/core/types"
 	"github.com/luxfi/geth/crypto"
-	"github.com/luxfi/geth/crypto/kzg4844"
+	"github.com/luxfi/crypto/kzg4844"
 	"github.com/luxfi/geth/params"
 	"github.com/holiman/uint256"
 	"go.uber.org/goleak"
