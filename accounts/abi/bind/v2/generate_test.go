c1cacf36a9209735ff3943d0211e9979ea80009c
Zach Kelling <z@zeekay.io>
2025-12-13 05:57:26 +0000

    Fix crypto type mismatch and abigen template issues
    - Fix cmd/abigen to use common.Keccak256Hash instead of crypto.Keccak256Hash
      (crypto.Hash.String() has no 0x prefix, causing wrong library pattern lookup)
    - Update abigen source templates to use luxfi/geth imports
    - Update testdata files to use luxfi/geth imports
    - Regenerate internal bindings with fixed library dependency references
    - Fix unused crypto imports in test files
    - Add common import to test cases using common.PubkeyToAddress
    - Fix cmd/ethkey and cmd/workload to use common.Keccak256Hash

--- accounts/abi/bind/v2/generate_test.go
+++ accounts/abi/bind/v2/generate_test.go
@@ -25,8 +25,8 @@ import (
 
 	"github.com/luxfi/geth/accounts/abi/abigen"
 	"github.com/luxfi/geth/cmd/utils"
+	"github.com/luxfi/geth/common"
 	"github.com/luxfi/geth/common/compiler"
-	"github.com/luxfi/crypto"
 )
 
 // Run go generate to recreate the test bindings.
--- triedb/pathdb/difflayer_test.go
+++ triedb/pathdb/difflayer_test.go
@@ -22,7 +22,6 @@ import (
 
 	"github.com/luxfi/geth/common"
 	"github.com/luxfi/geth/core/rawdb"
-	"github.com/luxfi/crypto"
 	"github.com/luxfi/geth/internal/testrand"
 	"github.com/luxfi/geth/trie/trienode"
 )
