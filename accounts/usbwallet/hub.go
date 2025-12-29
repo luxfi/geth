71695b02913ea5db2d4e5938540302caf5f81429
Zach Kelling <z@zeekay.ai>
2025-12-29 11:32:52 -0800

    chore: replace karalabe/hid with luxfi/hid
    Use luxfi/hid directly instead of karalabe/hid to unify
    HID libraries across the ecosystem and eliminate CGO
    symbol conflicts.

--- accounts/usbwallet/hub.go
+++ accounts/usbwallet/hub.go
@@ -26,7 +26,7 @@ import (
 	"github.com/luxfi/geth/accounts"
 	"github.com/luxfi/geth/event"
 	"github.com/luxfi/geth/log"
-	"github.com/karalabe/hid"
+	"github.com/luxfi/hid"
 )
 
 // LedgerScheme is the protocol scheme prefixing account and wallet URLs.
